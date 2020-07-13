cbf80036bcc8962ff62d5621cc0603a96e24705d
Khanarop <47390151+KhanaropThongplave@users.noreply.github.com>
2020-07-13 16:50:31 +0700

    initail commit

---
+++ app/models/tutorial.model.js
@@ -0,0 +1,20 @@
+// @ts-nocheck
+module.exports = mongoose => {
+    var schema = mongoose.Schema(
+      {
+        title: String,
+        description: String,
+        published: Boolean
+      },
+      { timestamps: true }
+    );
+  
+    schema.method("toJSON", function() {
+      const { __v, _id, ...object } = this.toObject();
+      object.id = _id;
+      return object;
+    });
+  
+    const Tutorial = mongoose.model("tutorial", schema);
+    return Tutorial;
+  };
